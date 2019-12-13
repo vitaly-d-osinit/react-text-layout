404cafc43277013e03c4ca989710b6c665cf5f1f
vitaly.demkin <vitaly.demkin@osinit.com>
2019-12-13 11:21:27 +0300

    Add material-ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,34 @@
-import * as React from "react";
+import React, { FunctionComponent } from "react";
 import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+
+import theme from "src/theme";
 
 type Props = {
   title?: string;
 };
 
-const Layout: React.FunctionComponent<Props> = ({
+const Layout: FunctionComponent<Props> = ({
   children,
   title = "React coding task «Text Layout»"
 }) => (
-  <div>
+  <>
+    <CssBaseline />
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
     </Head>
-    <header>Hello!</header>
-    {children}
-  </div>
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </>
 );
 
 export default Layout;
